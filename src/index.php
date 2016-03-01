7f7250a274bbc1c21087a3af7054aeebf0030c83
Pedro Albano <ei11016@fe.up.pt>
2016-03-01 15:44:13 +0000

    Added Bootstrap and Jquery Libraries Edited the Main page with a bootstrap example

--- src/index.php
+++ src/index.php
@@ -1,3 +1,38 @@
-<?php
-    echo("Place Holder");
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="js/bootstrap.js"></script>
+        <script src="js/jquery-1.12.1.js"></script>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        
+        <title>EventBook</title>        
+    </head>
+    <body>
+         <div class="container">
+            <div class="jumbotron">
+              <h1>My First Bootstrap Page</h1>
+              <p>Resize this responsive page to see the effect!</p>
+            </div>
+             
+            <div class="row">
+              <div class="col-sm-4">
+                <h3>Column 1</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+              </div>
+              <div class="col-sm-4">
+                <h3>Column 2</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+              </div>
+              <div class="col-sm-4">
+                <h3>Column 3</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+              </div>
+            </div>
+          </div>
+    </body>
+</html>
