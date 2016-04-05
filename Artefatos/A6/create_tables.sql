fb22af37539c7cfcb59786b79bab6577ffff5e7f
Pmma <Meeplee@users.noreply.github.com>
2016-04-05 16:47:27 +0100

    Added Create Tables Script

---
+++ Artefatos/A6/create_tables.sql
@@ -0,0 +1,137 @@
+DROP TABLE IF EXISTS Evento CASCADE;
+DROP TABLE IF EXISTS Utilizador CASCADE;
+DROP TABLE IF EXISTS Sondagem CASCADE;
+DROP TABLE IF EXISTS Opcao CASCADE;
+DROP TABLE IF EXISTS Comentario CASCADE;
+DROP TABLE IF EXISTS Pais CASCADE;
+DROP TABLE IF EXISTS Seguidor CASCADE;
+DROP TABLE IF EXISTS Participacao CASCADE;
+DROP TABLE IF EXISTS Convite CASCADE;
+DROP TABLE IF EXISTS Anfitriao CASCADE;
+DROP TABLE IF EXISTS UtilizadorOpcao CASCADE;
+DROP TABLE IF EXISTS SondagemOpcao CASCADE;
+DROP TABLE IF EXISTS ComentarioVoto CASCADE;
+DROP TABLE IF EXISTS Administrador CASCADE;
+DROP TABLE IF EXISTS Album CASCADE;
+DROP TABLE IF EXISTS Imagem CASCADE;
+
+-- Tomar atencao as relacoes, as referencias so podem ser criadas depois do referendo
+
+CREATE TABLE Evento (
+	idEvento SERIAL PRIMARY KEY,
+	titulo VARCHAR(100) NOT NULL,
+	capa TEXT,
+	descricao TEXT,
+	localizacao TEXT NOT NULL,
+	dataInicio DATE NOT NULL,
+	duracao INTEGER NOT NULL,
+	publico BOOLEAN DEFAULT TRUE NOT NULL
+);
+
+CREATE TABLE Pais(
+	idPais SERIAL PRIMARY KEY,
+	nome TEXT NOT NULL
+);
+
+CREATE TABLE Utilizador(
+	idUtilizador SERIAL PRIMARY KEY,
+	nome TEXT NOT NULL,
+	username VARCHAR(100) UNIQUE NOT NULL,
+	password VARCHAR(100) NOT NULL,
+	foto TEXT,
+	email TEXT UNIQUE NOT NULL,
+	idPais INTEGER REFERENCES Pais(idPais)
+);
+
+CREATE TABLE Sondagem(
+	idSondagem SERIAL PRIMARY KEY,
+	descricao TEXT,
+	"data" DATE NOT NULL,
+	idEvento INTEGER REFERENCES Evento(idEvento)
+);
+
+CREATE TABLE Opcao(
+	idOpcao SERIAL PRIMARY KEY,
+	descricao TEXT NOT NULL,
+	idSondagem INTEGER REFERENCES Sondagem(idSondagem)
+);
+
+CREATE TABLE Comentario(
+	idComentario SERIAL PRIMARY KEY,
+	texto TEXT NOT NULL,
+	"data" DATE NOT NULL,
+	idComentador INTEGER NOT NULL REFERENCES Utilizador(idUtilizador),
+	idEvento INTEGER NOT NULL REFERENCES Evento(idEvento),
+	idComentarioPai INTEGER REFERENCES Comentario(idComentario)
+);
+
+
+
+-- Constrain -> idSeguidor != idSeguido
+CREATE TABLE Seguidor(
+	idSeguidor INTEGER REFERENCES Utilizador(idUtilizador),
+	idSeguido INTEGER REFERENCES Utilizador(idUtilizador),
+	PRIMARY KEY(idSeguidor, idSeguido)
+);
+
+-- Constrain -> range da classificacao
+CREATE TABLE Participacao(
+	idEvento INTEGER REFERENCES Evento(idEvento),
+	idParticipante INTEGER REFERENCES Utilizador(idUtilizador),
+	classificacao INTEGER,
+	comentario TEXT,
+	PRIMARY KEY(idEvento, idParticipante)
+);
+
+CREATE TABLE Convite(
+	idEvento INTEGER REFERENCES Evento(idEvento),
+	idConvidado INTEGER REFERENCES Utilizador(idUtilizador),
+	"data" DATE NOT NULL,
+	resposta INTEGER NOT NULL,
+	PRIMARY KEY(idEvento, idConvidado)
+);
+
+CREATE TABLE Anfitriao(
+	idEvento INTEGER REFERENCES Evento(idEvento),
+	idAnfitriao INTEGER REFERENCES Utilizador(idUtilizador),
+	PRIMARY KEY(idEvento, idAnfitriao)
+);
+
+CREATE TABLE UtilizadorOpcao(
+	idUtilizador INTEGER REFERENCES Utilizador(idUtilizador),
+	idOpcao INTEGER REFERENCES Opcao(idOpcao),
+	PRIMARY KEY(idUtilizador, idOpcao)
+);
+
+CREATE TABLE SondagemOpcao(
+	idSondagem INTEGER REFERENCES Sondagem(idSondagem),
+	idOpcao INTEGER REFERENCES Opcao(idOpcao),
+	PRIMARY KEY(idSondagem, idOpcao)
+);
+
+CREATE TABLE ComentarioVoto(
+	idComentario INTEGER REFERENCES Comentario(idComentario),
+	idVotante INTEGER REFERENCES Utilizador(idUtilizador),
+	PRIMARY KEY(idComentario, idVotante)
+);
+
+CREATE TABLE Administrador(
+	idAdministrador SERIAL PRIMARY KEY,
+	username VARCHAR(100) UNIQUE NOT NULL,
+	password VARCHAR(100) NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL	
+);
+
+CREATE TABLE Album(
+	idAlbum SERIAL PRIMARY KEY,
+	nome TEXT NOT NULL,
+	descricao TEXT,
+	idEvento INTEGER REFERENCES Evento(idEvento)
+);
+
+CREATE TABLE Imagem(
+	idImagem SERIAL PRIMARY KEY,
+	caminho TEXT NOT NULL,
+	"data" DATE NOT NULL,
+	idAlbum INTEGER NOT NULL REFERENCES Album(idAlbum)
+);
