d7701d4140a41bcab33c31e2dc574d859b7603b8
Pmma <Meeplee@users.noreply.github.com>
2016-04-11 20:53:10 +0100

    Fixed Trigger Execution 1/2

--- Artefatos/A7/functions.sql
+++ Artefatos/A7/functions.sql
@@ -1,8 +1,6 @@
 -- Check if event is completed
 CREATE OR REPLACE FUNCTION check_event_complete(INTEGER) RETURNS BOOLEAN AS $$
-BEGIN
   SELECT EXISTS (
     SELECT idEvento, dataInicio, duracao FROM Evento WHERE idEvento = $1 AND dataInicio + (duracao * INTERVAL '1 second') >= CURRENT_TIMESTAMP
   );
-END
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE SQL;
