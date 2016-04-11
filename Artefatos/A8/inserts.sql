73e0e8ecb2e083797c681d0bcdc5b255cafe1a15
Hugo Drumond <hugo.drumond@fe.up.pt>
2016-04-11 19:24:51 +0100

    Inserts done by hugo and diogo pereira. Someone please test them.

---
+++ Artefatos/A8/inserts.sql
@@ -0,0 +1,374 @@
+-- Tabela "Evento"
+
+INSERT INTO evento (idEvento, titulo, capa, descricao, localizacao, dataInicio, duracao, publico) VALUES (1, 'Be better be ', 'badjoras.jpg', '"water my friend" Bruce Lee', 'FEUP', '2016-04-18', 60, TRUE);
+
+INSERT INTO evento (idEvento, titulo, capa, descricao, localizacao, dataInicio, duracao, publico) VALUES (2, 'The Great Artists', 'progrockepicmusic.jpg', 'The best Prog Rock and Epic Music', 'Viana', '2016-06-05', 180, TRUE);
+
+INSERT INTO evento (idEvento, titulo, capa, descricao, localizacao, dataInicio, duracao, publico) VALUES (3, 'CARL COX', 'carlcox.jpg', 'Bailarico', 'Viana', '2016-02-01', 150, TRUE);
+
+-- Tabela "Utilizador"
+
+INSERT INTO utilizador (nome, username, password, foto, email, IdPais) VALUES ('Xavi', 'diogodometro', 'lbaw1522', 'metro.jpg', 'diogoxavier95@hotmail.com', 177);
+
+INSERT INTO utilizador (nome, username, password, foto, email, IdPais) VALUES ('Diogo', 'avc', 'schwein', 'schwein.jpg', 'avc@hotmail.com', 1);
+
+INSERT INTO utilizador (nome, username, password, foto, email, IdPais) VALUES ('Hugo', 'drumond', 'fruhstuck', 'drumond.jpg', 'drumond@hotmail.com', 6);
+
+-- Tabela "Sondagem"
+
+INSERT INTO sondagem (idOpcao, descricao, data, escolhaMultipla, IdEvento) VALUES (1, 'Onde realizar?', '2016-04-01', FALSE, 1);
+
+INSERT INTO sondagem (idOpcao, descricao, data, escolhaMultipla, IdEvento) VALUES (2, 'Prog Rock or Epic?', '2016-05-12', FALSE, 2);
+
+INSERT INTO sondagem (idOpcao, descricao, data, escolhaMultipla, IdEvento) VALUES (3, 'Bebidas?', '2016-01-15', TRUE, 3);
+
+-- Tabela "Opcao"
+
+INSERT INTO opcao (idOpcao, descricao, IdSondagem) VALUES (1, 'Queijos', 1);
+
+INSERT INTO opcao (idOpcao, descricao, IdSondagem) VALUES (2, 'B003', 1);
+
+INSERT INTO opcao (idOpcao, descricao, IdSondagem) VALUES (3, 'B330', 1);
+
+INSERT INTO opcao (idOpcao, descricao, IdSondagem) VALUES (4, 'Biblioteca', 1);
+
+INSERT INTO opcao (idOpcao, descricao, idSondagem) VALUES (5, 'Prog Rock', 2);
+INSERT INTO opcao (idOpcao, descricao, idSondagem) VALUES (6, 'Epic', 2);
+
+INSERT INTO opcao (idOpcao, descricao, idSondagem) VALUES (7, 'Laranjada', 3);
+
+INSERT INTO opcao (idOpcao, descricao, idSondagem) VALUES (8, 'Compal de ananás', 3);
+
+INSERT INTO opcao (idOpcao, descricao, idSondagem) VALUES (9, 'Vodka', 3);
+
+INSERT INTO opcao (idOpcao, descricao, idSondagem) VALUES (10, 'Água', 3);
+
+-- Tabela "Comentario"
+
+INSERT INTO comentario (IdComentario, texto, data, IdComentador, IdEvento, IdComentarioPai) VALUES (1, 'LoLOlOLolOloLol', '2016-05-01', 2, 1, NULL);
+
+INSERT INTO comentario (IdComentario, texto, data, IdComentador, IdEvento, IdComentarioPai) VALUES (2, 'tas lixado', '2016-05-01', 1, 1, 1);
+
+INSERT INTO comentario (IdComentario, texto, data, IdComentador, IdEvento, IdComentarioPai) VALUES (3, 'nao estou nao lol', '2016-5-4', 2, 1, 1);
+
+INSERT INTO comentario (IdComentario, texto, data, IdComentador, IdEvento, IdComentarioPai) VALUES (4, 'ahahahah que riso estes dois', '2016-5-4', 3, 1, 1);
+
+-- Tabela "Seguidor"
+
+INSERT INTO seguidor (IdSeguidor, IdSeguido, data) VALUES (2, 1, '2016-06-03');
+
+INSERT INTO seguidor (IdSeguidor, IdSeguido, data) VALUES (3, 1, '2016-06-18');
+
+INSERT INTO seguidor (IdSeguidor, IdSeguido, data) VALUES (2, 3, '2016-06-20');
+
+-- Tabela "Participacao"
+
+INSERT INTO participacao (IdEvento, IdParticipante, classificacao, comentario) VALUES (2, 2, 4, 'Adorei!');
+
+INSERT INTO participacao (IdEvento, IdParticipante, classificacao, comentario) VALUES (1, 3, 3, 'Nem me aqueceu nem arrefeceu!');
+
+INSERT INTO participacao (IdEvento, IdParticipante, classificacao, comentario) VALUES (3, 1, 0, 'Jamais voltarei a pôr os pés num evento deste género!');
+
+-- Tabela "Convite"
+
+INSERT INTO convite (IdEvento, IdConvidado, data, resposta) VALUES (1, 2, '2016-04-20', NULL);
+
+INSERT INTO convite (IdEvento, IdConvidado, data, resposta) VALUES (2, 3, '2016-06-24', TRUE);
+
+INSERT INTO convite (IdEvento, IdConvidado, data, resposta) VALUES (3, 1, '2016-5-1', FALSE);
+
+-- Tabela "Anfitriao"
+
+INSERT INTO Anfitriao (IdEvento, IdAnfitriao) VALUES (1, 2);
+INSERT INTO Anfitriao (IdEvento, IdAnfitriao) VALUES (2, 1);
+INSERT INTO Anfitriao (IdEvento, IdAnfitriao) VALUES (3, 3);
+
+-- Tabela "UtilizadorOpcao"
+
+INSERT INTO utilizadoropcao (IdUtilizador, IdOpcao) VALUES (1, 3);
+INSERT INTO utilizadoropcao (IdUtilizador, IdOpcao) VALUES (2, 5);
+INSERT INTO utilizadoropcao (IdUtilizador, IdOpcao) VALUES (3, 1);
+INSERT INTO utilizadoropcao (IdUtilizador, IdOpcao) VALUES (3, 8);
+
+-- Tabela "ComentarioVoto"
+
+INSERT INTO comentariovoto (IdComentario, IdVotante, positivo) VALUES (2, 2, TRUE);
+INSERT INTO comentariovoto (IdComentario, IdVotante, positivo) VALUES (3, 1, TRUE);
+INSERT INTO comentariovoto (IdComentario, IdVotante, positivo) VALUES (4, 2, FALSE);
+
+-- Tabela "Pais"
+
+INSERT INTO pais (nome) VALUES ('Afghanistan');
+INSERT INTO pais (nome) VALUES ('Albania');
+INSERT INTO pais (nome) VALUES ('Algeria');
+INSERT INTO pais (nome) VALUES ('American Samoa');
+INSERT INTO pais (nome) VALUES ('Andorra');
+INSERT INTO pais (nome) VALUES ('Angola');
+INSERT INTO pais (nome) VALUES ('Anguilla');
+INSERT INTO pais (nome) VALUES ('Antarctica');
+INSERT INTO pais (nome) VALUES ('Antigua and Barbuda');
+INSERT INTO pais (nome) VALUES ('Argentina');
+INSERT INTO pais (nome) VALUES ('Armenia');
+INSERT INTO pais (nome) VALUES ('Armenia');
+INSERT INTO pais (nome) VALUES ('Aruba');
+INSERT INTO pais (nome) VALUES ('Australia');
+INSERT INTO pais (nome) VALUES ('Austria');
+INSERT INTO pais (nome) VALUES ('Azerbaijan');
+INSERT INTO pais (nome) VALUES ('Azerbaijan');
+INSERT INTO pais (nome) VALUES ('Bahamas');
+INSERT INTO pais (nome) VALUES ('Bahrain');
+INSERT INTO pais (nome) VALUES ('Bangladesh');
+INSERT INTO pais (nome) VALUES ('Barbados');
+INSERT INTO pais (nome) VALUES ('Belarus');
+INSERT INTO pais (nome) VALUES ('Belgium');
+INSERT INTO pais (nome) VALUES ('Belize');
+INSERT INTO pais (nome) VALUES ('Benin');
+INSERT INTO pais (nome) VALUES ('Bermuda');
+INSERT INTO pais (nome) VALUES ('Bhutan');
+INSERT INTO pais (nome) VALUES ('Bolivia');
+INSERT INTO pais (nome) VALUES ('Bosnia and Herzegovina');
+INSERT INTO pais (nome) VALUES ('Botswana');
+INSERT INTO pais (nome) VALUES ('Bouvet Island');
+INSERT INTO pais (nome) VALUES ('Brazil');
+INSERT INTO pais (nome) VALUES ('British Indian Ocean Territory');
+INSERT INTO pais (nome) VALUES ('Brunei Darussalam');
+INSERT INTO pais (nome) VALUES ('Bulgaria');
+INSERT INTO pais (nome) VALUES ('Burkina Faso');
+INSERT INTO pais (nome) VALUES ('Burundi');
+INSERT INTO pais (nome) VALUES ('Cambodia');
+INSERT INTO pais (nome) VALUES ('Cameroon');
+INSERT INTO pais (nome) VALUES ('Canada');
+INSERT INTO pais (nome) VALUES ('Cape Verde');
+INSERT INTO pais (nome) VALUES ('Cayman Islands');
+INSERT INTO pais (nome) VALUES ('Central African Republic');
+INSERT INTO pais (nome) VALUES ('Chad');
+INSERT INTO pais (nome) VALUES ('Chile');
+INSERT INTO pais (nome) VALUES ('China');
+INSERT INTO pais (nome) VALUES ('Christmas Island');
+INSERT INTO pais (nome) VALUES ('Cocos INSERT INTO pais (nome) VALUES (Keeling) Islands');
+INSERT INTO pais (nome) VALUES ('Colombia');
+INSERT INTO pais (nome) VALUES ('Comoros');
+INSERT INTO pais (nome) VALUES ('Congo');
+INSERT INTO pais (nome) VALUES ('Congo, The Democratic Republic of The');
+INSERT INTO pais (nome) VALUES ('Cook Islands');
+INSERT INTO pais (nome) VALUES ('Costa Rica');
+INSERT INTO pais (nome) VALUES ('Cote D''ivoire');
+INSERT INTO pais (nome) VALUES ('Croatia');
+INSERT INTO pais (nome) VALUES ('Cuba');
+INSERT INTO pais (nome) VALUES ('Cyprus');
+INSERT INTO pais (nome) VALUES ('Czech Republic');
+INSERT INTO pais (nome) VALUES ('Denmark');
+INSERT INTO pais (nome) VALUES ('Djibouti');
+INSERT INTO pais (nome) VALUES ('Dominica');
+INSERT INTO pais (nome) VALUES ('Dominican Republic');
+INSERT INTO pais (nome) VALUES ('Easter Island');
+INSERT INTO pais (nome) VALUES ('Ecuador');
+INSERT INTO pais (nome) VALUES ('Egypt');
+INSERT INTO pais (nome) VALUES ('El Salvador');
+INSERT INTO pais (nome) VALUES ('Equatorial Guinea');
+INSERT INTO pais (nome) VALUES ('Eritrea');
+INSERT INTO pais (nome) VALUES ('Estonia');
+INSERT INTO pais (nome) VALUES ('Ethiopia');
+INSERT INTO pais (nome) VALUES ('Falkland Islands INSERT INTO pais (nome) VALUES (Malvinas)');
+INSERT INTO pais (nome) VALUES ('Faroe Islands');
+INSERT INTO pais (nome) VALUES ('Fiji');
+INSERT INTO pais (nome) VALUES ('Finland');
+INSERT INTO pais (nome) VALUES ('France');
+INSERT INTO pais (nome) VALUES ('French Guiana');
+INSERT INTO pais (nome) VALUES ('French Polynesia');
+INSERT INTO pais (nome) VALUES ('French Southern Territories');
+INSERT INTO pais (nome) VALUES ('Gabon');
+INSERT INTO pais (nome) VALUES ('Gambia');
+INSERT INTO pais (nome) VALUES ('Georgia');
+INSERT INTO pais (nome) VALUES ('Germany');
+INSERT INTO pais (nome) VALUES ('Ghana');
+INSERT INTO pais (nome) VALUES ('Gibraltar');
+INSERT INTO pais (nome) VALUES ('Greece');
+INSERT INTO pais (nome) VALUES ('Greenland');
+INSERT INTO pais (nome) VALUES ('Grenada');
+INSERT INTO pais (nome) VALUES ('Guadeloupe');
+INSERT INTO pais (nome) VALUES ('Guam');
+INSERT INTO pais (nome) VALUES ('Guatemala');
+INSERT INTO pais (nome) VALUES ('Guinea');
+INSERT INTO pais (nome) VALUES ('Guinea-bissau');
+INSERT INTO pais (nome) VALUES ('Guyana');
+INSERT INTO pais (nome) VALUES ('Haiti');
+INSERT INTO pais (nome) VALUES ('Heard Island and Mcdonald Islands');
+INSERT INTO pais (nome) VALUES ('Honduras');
+INSERT INTO pais (nome) VALUES ('Hong Kong');
+INSERT INTO pais (nome) VALUES ('Hungary');
+INSERT INTO pais (nome) VALUES ('Iceland');
+INSERT INTO pais (nome) VALUES ('India');
+INSERT INTO pais (nome) VALUES ('Indonesia');
+INSERT INTO pais (nome) VALUES ('Indonesia');
+INSERT INTO pais (nome) VALUES ('Iran');
+INSERT INTO pais (nome) VALUES ('Iraq');
+INSERT INTO pais (nome) VALUES ('Ireland');
+INSERT INTO pais (nome) VALUES ('Israel');
+INSERT INTO pais (nome) VALUES ('Italy');
+INSERT INTO pais (nome) VALUES ('Jamaica');
+INSERT INTO pais (nome) VALUES ('Japan');
+INSERT INTO pais (nome) VALUES ('Jordan');
+INSERT INTO pais (nome) VALUES ('Kazakhstan');
+INSERT INTO pais (nome) VALUES ('Kazakhstan');
+INSERT INTO pais (nome) VALUES ('Kenya');
+INSERT INTO pais (nome) VALUES ('Kiribati');
+INSERT INTO pais (nome) VALUES ('Korea, North');
+INSERT INTO pais (nome) VALUES ('Korea, South');
+INSERT INTO pais (nome) VALUES ('Kosovo');
+INSERT INTO pais (nome) VALUES ('Kuwait');
+INSERT INTO pais (nome) VALUES ('Kyrgyzstan');
+INSERT INTO pais (nome) VALUES ('Laos');
+INSERT INTO pais (nome) VALUES ('Latvia');
+INSERT INTO pais (nome) VALUES ('Lebanon');
+INSERT INTO pais (nome) VALUES ('Lesotho');
+INSERT INTO pais (nome) VALUES ('Liberia');
+INSERT INTO pais (nome) VALUES ('Libyan Arab Jamahiriya');
+INSERT INTO pais (nome) VALUES ('Liechtenstein');
+INSERT INTO pais (nome) VALUES ('Lithuania');
+INSERT INTO pais (nome) VALUES ('Luxembourg');
+INSERT INTO pais (nome) VALUES ('Macau');
+INSERT INTO pais (nome) VALUES ('Macedonia');
+INSERT INTO pais (nome) VALUES ('Madagascar');
+INSERT INTO pais (nome) VALUES ('Malawi');
+INSERT INTO pais (nome) VALUES ('Malaysia');
+INSERT INTO pais (nome) VALUES ('Maldives');
+INSERT INTO pais (nome) VALUES ('Mali');
+INSERT INTO pais (nome) VALUES ('Malta');
+INSERT INTO pais (nome) VALUES ('Marshall Islands');
+INSERT INTO pais (nome) VALUES ('Martinique');
+INSERT INTO pais (nome) VALUES ('Mauritania');
+INSERT INTO pais (nome) VALUES ('Mauritius');
+INSERT INTO pais (nome) VALUES ('Mayotte');
+INSERT INTO pais (nome) VALUES ('Mexico');
+INSERT INTO pais (nome) VALUES ('Micronesia, Federated States of');
+INSERT INTO pais (nome) VALUES ('Moldova, Republic of');
+INSERT INTO pais (nome) VALUES ('Monaco');
+INSERT INTO pais (nome) VALUES ('Mongolia');
+INSERT INTO pais (nome) VALUES ('Montenegro');
+INSERT INTO pais (nome) VALUES ('Montserrat');
+INSERT INTO pais (nome) VALUES ('Morocco');
+INSERT INTO pais (nome) VALUES ('Mozambique');
+INSERT INTO pais (nome) VALUES ('Myanmar');
+INSERT INTO pais (nome) VALUES ('Namibia');
+INSERT INTO pais (nome) VALUES ('Nauru');
+INSERT INTO pais (nome) VALUES ('Nepal');
+INSERT INTO pais (nome) VALUES ('Netherlands');
+INSERT INTO pais (nome) VALUES ('Netherlands Antilles');
+INSERT INTO pais (nome) VALUES ('New Caledonia');
+INSERT INTO pais (nome) VALUES ('New Zealand');
+INSERT INTO pais (nome) VALUES ('Nicaragua');
+INSERT INTO pais (nome) VALUES ('Niger');
+INSERT INTO pais (nome) VALUES ('Nigeria');
+INSERT INTO pais (nome) VALUES ('Niue');
+INSERT INTO pais (nome) VALUES ('Norfolk Island');
+INSERT INTO pais (nome) VALUES ('Northern Mariana Islands');
+INSERT INTO pais (nome) VALUES ('Norway');
+INSERT INTO pais (nome) VALUES ('Oman');
+INSERT INTO pais (nome) VALUES ('Pakistan');
+INSERT INTO pais (nome) VALUES ('Palau');
+INSERT INTO pais (nome) VALUES ('Palestinian Territory');
+INSERT INTO pais (nome) VALUES ('Panama');
+INSERT INTO pais (nome) VALUES ('Papua New Guinea');
+INSERT INTO pais (nome) VALUES ('Paraguay');
+INSERT INTO pais (nome) VALUES ('Peru');
+INSERT INTO pais (nome) VALUES ('Philippines');
+INSERT INTO pais (nome) VALUES ('Pitcairn');
+INSERT INTO pais (nome) VALUES ('Poland');
+INSERT INTO pais (nome) VALUES ('Portugal');
+INSERT INTO pais (nome) VALUES ('Puerto Rico');
+INSERT INTO pais (nome) VALUES ('Qatar');
+INSERT INTO pais (nome) VALUES ('Reunion');
+INSERT INTO pais (nome) VALUES ('Romania');
+INSERT INTO pais (nome) VALUES ('Russia');
+INSERT INTO pais (nome) VALUES ('Russia');
+INSERT INTO pais (nome) VALUES ('Rwanda');
+INSERT INTO pais (nome) VALUES ('Saint Helena');
+INSERT INTO pais (nome) VALUES ('Saint Kitts and Nevis');
+INSERT INTO pais (nome) VALUES ('Saint Lucia');
+INSERT INTO pais (nome) VALUES ('Saint Pierre and Miquelon');
+INSERT INTO pais (nome) VALUES ('Saint Vincent and The Grenadines');
+INSERT INTO pais (nome) VALUES ('Samoa');
+INSERT INTO pais (nome) VALUES ('San Marino');
+INSERT INTO pais (nome) VALUES ('Sao Tome and Principe');
+INSERT INTO pais (nome) VALUES ('Saudi Arabia');
+INSERT INTO pais (nome) VALUES ('Senegal');
+INSERT INTO pais (nome) VALUES ('Serbia and Montenegro');
+INSERT INTO pais (nome) VALUES ('Seychelles');
+INSERT INTO pais (nome) VALUES ('Sierra Leone');
+INSERT INTO pais (nome) VALUES ('Singapore');
+INSERT INTO pais (nome) VALUES ('Slovakia');
+INSERT INTO pais (nome) VALUES ('Slovenia');
+INSERT INTO pais (nome) VALUES ('Solomon Islands');
+INSERT INTO pais (nome) VALUES ('Somalia');
+INSERT INTO pais (nome) VALUES ('South Africa');
+INSERT INTO pais (nome) VALUES ('South Georgia and The South Sandwich Islands');
+INSERT INTO pais (nome) VALUES ('Spain');
+INSERT INTO pais (nome) VALUES ('Sri Lanka');
+INSERT INTO pais (nome) VALUES ('Sudan');
+INSERT INTO pais (nome) VALUES ('Suriname');
+INSERT INTO pais (nome) VALUES ('Svalbard and Jan Mayen');
+INSERT INTO pais (nome) VALUES ('Swaziland');
+INSERT INTO pais (nome) VALUES ('Sweden');
+INSERT INTO pais (nome) VALUES ('Switzerland');
+INSERT INTO pais (nome) VALUES ('Syria');
+INSERT INTO pais (nome) VALUES ('Taiwan');
+INSERT INTO pais (nome) VALUES ('Tajikistan');
+INSERT INTO pais (nome) VALUES ('Tanzania, United Republic of');
+INSERT INTO pais (nome) VALUES ('Thailand');
+INSERT INTO pais (nome) VALUES ('Timor-leste');
+INSERT INTO pais (nome) VALUES ('Togo');
+INSERT INTO pais (nome) VALUES ('Tokelau');
+INSERT INTO pais (nome) VALUES ('Tonga');
+INSERT INTO pais (nome) VALUES ('Trinidad and Tobago');
+INSERT INTO pais (nome) VALUES ('Tunisia');
+INSERT INTO pais (nome) VALUES ('Turkey');
+INSERT INTO pais (nome) VALUES ('Turkey');
+INSERT INTO pais (nome) VALUES ('Turkmenistan');
+INSERT INTO pais (nome) VALUES ('Turks and Caicos Islands');
+INSERT INTO pais (nome) VALUES ('Tuvalu');
+INSERT INTO pais (nome) VALUES ('Uganda');
+INSERT INTO pais (nome) VALUES ('Ukraine');
+INSERT INTO pais (nome) VALUES ('United Arab Emirates');
+INSERT INTO pais (nome) VALUES ('United Kingdom');
+INSERT INTO pais (nome) VALUES ('United States');
+INSERT INTO pais (nome) VALUES ('United States Minor Outlying Islands');
+INSERT INTO pais (nome) VALUES ('Uruguay');
+INSERT INTO pais (nome) VALUES ('Uzbekistan');
+INSERT INTO pais (nome) VALUES ('Vanuatu');
+INSERT INTO pais (nome) VALUES ('Vatican City');
+INSERT INTO pais (nome) VALUES ('Venezuela');
+INSERT INTO pais (nome) VALUES ('Vietnam');
+INSERT INTO pais (nome) VALUES ('Virgin Islands, British');
+INSERT INTO pais (nome) VALUES ('Virgin Islands, U.S.');
+INSERT INTO pais (nome) VALUES ('Wallis and Futuna');
+INSERT INTO pais (nome) VALUES ('Western Sahara');
+INSERT INTO pais (nome) VALUES ('Yemen');
+INSERT INTO pais (nome) VALUES ('Yemen');
+INSERT INTO pais (nome) VALUES ('Zambia');
+INSERT INTO pais (nome) VALUES ('Zimbabwe');
+
+-- Tabela "Album"
+INSERT INTO album (idAlbum, nome, descricao, idEvento) VALUES(1, 'First Session', 'Brainstorming and Meetup', 1);
+INSERT INTO album (idAlbum, nome, descricao, idEvento) VALUES(2, 'Second Session', 'Games', 1);
+INSERT INTO album (idAlbum, nome, descricao, idEvento) VALUES(3, 'Third Session', 'Bye bye' , 1);
+INSERT INTO album (idAlbum, nome, descricao, idEvento) VALUES(4, 'Progressive Rock', 'Pink Floyd, Led Zeppelin, King Crimson, Camel', 2);
+INSERT INTO album (idAlbum, descricao, idEvento) VALUES(5, 'Epic Music', 'Hans Zimmer', 'Thomas Bergersen, Audiomachine, Two Steps From Hell', 2);
+INSERT INTO album (idAlbum, nome, descricao, idEvento) VALUES(6, 'Malucos', 'Os loucos da festa', 3);
+
+-- Tabela "Imagem"
+INSERT INTO imagem(caminho, data, idAlbum) VALUES('session1.jpg', 2016-04-12, 1);
+INSERT INTO imagem(caminho, data, idAlbum) VALUES('session2.jpg', 2016-04-13, 2);
+INSERT INTO imagem(caminho, data, idAlbum) VALUES('session3.jpg', 2016-04-14, 3);
+INSERT INTO imagem(caminho, data, idAlbum) VALUES('pinkfloyd-rocking.jpg', 2016-05-12, 4);
+INSERT INTO imagem(caminho, data, idAlbum) VALUES('ledzeppelin-rocking.jpg', 2016-05-12, 4);
+INSERT INTO imagem(caminho, data, idAlbum) VALUES('hans-zimmer.jpg', 2016-07-10, 5);
+INSERT INTO imagem(caminho, data, idAlbum) VALUES('audiomachine.jpg', 2016-07-13, 5);
+INSERT INTO imagem(caminho, data, idAlbum) VALUES('audiomachine.jpg', 2016-07-14, 5);
+INSERT INTO imagem(caminho, data, idAlbum) VALUES('joaquim.jpg', 2016-02-01, 6);
+INSERT INTO imagem(caminho, data, idAlbum) VALUES('alberto.jpg', 2016-02-09, 6);
+INSERT INTO imagem(caminho, data, idAlbum) VALUES('gertrudes.jpg', 2016-02-14, 6);
+
+-- Tabela "Administrador
+INSERT INTO Administrador (username, email, password) VALUES ('theboss', 'theboss@gg.bb', 'queriasbatatasfrias')
+INSERT INTO Administrador(username, email, password) VALUES('magical', 'unicorn@blue.sky', '12314nanananana')
