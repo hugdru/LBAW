de49c53bdefd333cf89333c71375e90e34cf9102
Pmma <Meeplee@users.noreply.github.com>
2016-04-11 20:30:30 +0100

    Re-fixed

--- Artefatos/A7/functions.sql
+++ Artefatos/A7/functions.sql
@@ -1,9 +1,7 @@
 -- Check if event is completed
 CREATE OR REPLACE FUNCTION check_event_complete(INTEGER) RETURNS BOOLEAN AS $$
-BEGIN
   SELECT EXISTS (
     SELECT idEvento, dataInicio, duracao FROM Evento WHERE idEvento = $1 AND dataInicio + (duracao * INTERVAL '1 second') >= CURRENT_TIMESTAMP
   );
-END
 $$ LANGUAGE SQL;
 
