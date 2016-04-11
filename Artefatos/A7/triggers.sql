f8638bddf99a47c7af7945454c728e5c049fe6e2
Pmma <Meeplee@users.noreply.github.com>
2016-04-11 20:53:45 +0100

    Fixed Trigger Execution 2/2

--- Artefatos/A7/triggers.sql
+++ Artefatos/A7/triggers.sql
@@ -3,10 +3,13 @@ DROP TRIGGER IF EXISTS CanRate ON Participacao;
 
 CREATE OR REPLACE FUNCTION trigger_canRate() RETURNS TRIGGER AS $$
 BEGIN
-  IF NOT check_event_complete(New.eventId) THEN
+  IF NOT check_event_complete(New.idEvento) THEN
     RAISE EXCEPTION 'Event is not complete yet';
   END IF;
+  RETURN NEW;
 END
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER CanRate BEFORE INSERT ON Participacao EXECUTE PROCEDURE trigger_canRate();
+CREATE TRIGGER CanRate BEFORE INSERT ON Participacao 
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_canRate();
