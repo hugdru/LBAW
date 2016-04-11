46e3b7b99bb66090999ec4c1ec16acbcc215dd71
Pmma <Meeplee@users.noreply.github.com>
2016-04-11 20:33:46 +0100

    Reverted Fixes, Because they didn't fix anything

--- Artefatos/A7/functions.sql
+++ Artefatos/A7/functions.sql
@@ -1,7 +1,8 @@
 -- Check if event is completed
 CREATE OR REPLACE FUNCTION check_event_complete(INTEGER) RETURNS BOOLEAN AS $$
+BEGIN
   SELECT EXISTS (
     SELECT idEvento, dataInicio, duracao FROM Evento WHERE idEvento = $1 AND dataInicio + (duracao * INTERVAL '1 second') >= CURRENT_TIMESTAMP
   );
-$$ LANGUAGE SQL;
-
+END
+$$ LANGUAGE plpgsql;
