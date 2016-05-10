f59a0e71df9e16f432dfd494c83702d564b505cb
DiogoXRP <DiogoXRP@github.com>
2016-05-10 16:44:03 +0100

    minor fix

--- Artefatos/A10/pages/home.php
+++ Artefatos/A10/pages/home.php
@@ -1,6 +1,6 @@
 <?php
     include_once('../config/init.php');
-    include_once($BASE_DIR .'database/user.php');
+    include_once($BASE_DIR .'database/users.php');
 
     //Yet another Gnomo.fe.up.pt fix
     //Fixes session default location being unaccessable.
