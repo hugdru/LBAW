b61d61b271135d9ee307acca24a0403e50ca5c15
Hugo Drumond <hugo.drumond@fe.up.pt>
2016-06-04 20:45:48 +0100

    init proto to final fix in base dir

--- Artefatos/A12/config/init.php
+++ Artefatos/A12/config/init.php
@@ -6,7 +6,7 @@ secure_session_start("/~lbaw1522");
 
 error_reporting(E_ERROR | E_WARNING); // E_NOTICE by default
 
-$BASE_DIR = '/opt/lbaw/lbaw1522/public_html/proto/';
+$BASE_DIR = '/opt/lbaw/lbaw1522/public_html/final/';
 $BASE_URL = '/~lbaw1522/final/';
 
 $conn = new PDO('pgsql:host=dbm.fe.up.pt;dbname=lbaw1522', 'lbaw1522', 'snJa!*cBqPJ');
