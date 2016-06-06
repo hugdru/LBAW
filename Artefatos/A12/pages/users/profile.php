86688f73ea3cef11102a5b28a31bd8a3bc7ae8b4
Pmma <pedropmma@gmail.com>
2016-06-06 22:57:44 +0100

    Profile Page now accepts user id via GET, displays self if missing or invalid

--- Artefatos/A12/pages/users/profile.php
+++ Artefatos/A12/pages/users/profile.php
@@ -5,7 +5,14 @@ include_once($BASE_DIR . 'functions/users.php');
 
 redirectIfNotLoggedIn($BASE_URL . "pages/users/login.php");
 
-
+if($_GET["id"]){
+    $user = getUserById($_GET["id"]);
+    
+    if($user){
+        $user["pais"] = getCountryById($user["idpais"]);
+    }
+    $smarty->assign("user", $user);
+}
 
 $smarty->assign('currentPage', "profile");
 $smarty->display('users/profile.tpl');
